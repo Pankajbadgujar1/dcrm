78b365db377164a86597083c66314ef93ce14f8b
Demo-1 <pankajbadgujar799@gmail.com>
2024-02-01 23:25:37 +0530

    add login page

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.home, name='home'),
-    
+    #path('login/',views.login_user,name=login),
+   # path('')
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    #to check see login in
+    if request.method == "POST":
+        username = request.POST["username"]
+    
+        password = request.POST["password"]
+        
+
+        user = authenticate(username='username',password= password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            messages.success(request,"you have successfully log in !")
+            redirect('home')
+
+        else:
+            messages.success(request,"Errer while you are login ..Please try again")
+            
+    else:
+        return render(request, 'home.html')
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
